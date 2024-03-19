fc72c3d2b2368b5f67746881504cc89f9c22c75b
Cecilia <lcecilia8147@gmail.com>
2024-03-19 15:42:13 -0300

    se agrega para validar email y contraseña en archivo js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /** Validar email*/ 
+let usuario ; 
+let boton = document.getElementsByClassName("btnLogin");
+
+boton[0].addEventListener("click", leer);
+
+function leer  () {
+    usuario = document.querySelector(".emailInput").value ;
+    password = document.querySelector(".passwordInput").value;
+    
+    if(usuario == "1234" ){
+        alert("usuario Correcto");
+    }
+}
+

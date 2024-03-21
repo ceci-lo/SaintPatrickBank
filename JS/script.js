5461309d7ccc2f5f8a1574ec9b67af74184d667f
Cecilia <lcecilia8147@gmail.com>
2024-03-21 14:28:14 -0300

    Se arregla carga de json

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
-/** Validar usuario correcto*/ 
-let usuario ; 
+/** Validar usuario correcto*/
+let usuario;
 let usuarioArray = [];
 let passwordArray = [];
 
@@ -7,35 +7,37 @@ let boton = document.getElementsByClassName("btnLogin");
 
 
 boton[0].addEventListener("click", cargarJson);
+cargarJson();
 boton[0].addEventListener("click", leer);
 /** Trae los usuarios del Json */
 
 
-function cargarJson () {
-    fetch('tarjetas.json')
-    .then(function(respuesta){ // me conecta con el json
-        return respuesta.json();
-    })
-    .then(function(data){ //me trae los datos de json
-      
-       data.forEach(function(usuario){
-        usuarioArray.push(usuario.usuario); 
-        passwordArray.push(usuario.password);
-       });
-    })
-  
+function cargarJson() {
+    fetch('../tarjetas.json')
+        .then(function (respuesta) { // me conecta con el json
+            return respuesta.json();
+        })
+        .then(function (data) { //me trae los datos de json
+
+            data.forEach(function (usuario) {
+                usuarioArray.push(usuario.usuario);
+                passwordArray.push(usuario.password);
+            })      
+           
+        })
+
 }
 
 /** Busca un usuario de JSON y compara con lo que puso el usuario
  * return boolean 
  * parametro usuario puesto por el usuario 
  */
-function  usuarioRegistrado(usuario){
+function usuarioRegistrado(usuario) {
     let usuarioEncontrado = false;
 
     for (let i = 0; i < usuarioArray.length; i++) {
-        if(usuario == usuarioArray[i]){
-        usuarioEncontrado = true;
+        if (usuario == usuarioArray[i]) {
+            usuarioEncontrado = true;
         }
     }
 
@@ -49,8 +51,8 @@ function  usuarioRegistrado(usuario){
 const passwordResgistrada = (password) => {
     let passwordEncontrada = false;
 
-    for (let i = 0 ; i < passwordArray.length; i++){
-        if(password == passwordArray[i]){
+    for (let i = 0; i < passwordArray.length; i++) {
+        if (password == passwordArray[i]) {
             passwordEncontrada = true;
         }
     }
@@ -59,17 +61,17 @@ const passwordResgistrada = (password) => {
 
 
 
-function leer  () {
-    usuario = document.querySelector(".emailInput").value ;
+function leer() {
+    usuario = document.querySelector(".emailInput").value;
     password = document.querySelector(".passwordInput").value;
-    
-    if(usuarioRegistrado(usuario) && passwordResgistrada(password) ){
+
+    if (usuarioRegistrado(usuario) && passwordResgistrada(password)) {
         //Aca deberia poner el link a otra seccion permitiendo ingresar al sitio si es true
         // pedir los datos de nuevo si es false
-        alert('El usuario puede ingresar')
-       }else {
-         alert('ingrese los datos nuevamente, el usuario o conseña es incorrecto');
-     }
+        alert('El usuario puede ingresar');
+    } else {
+        alert('ingrese los datos nuevamente, el usuario o conseña es incorrecto');
+    }
 }
 
 // https://www.youtube.com/watch?v=xqBvtvXh9Z4
